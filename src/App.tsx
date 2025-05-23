c7390349162f791b5b8e1045efdc7159eb7ae257
Bularga Alexandru <bularga.alexander@gmail.com>
2025-05-23 12:07:50 +0300

    feat: Added a test version of x-calendar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import { CalendarIndex } from "./components/calendar/CalendarIndex";
 import { Scanner } from "./components/Scanner";
 
 function App() {
   return (
     <div>
+      <CalendarIndex />
       <Scanner onScan={(img) => console.log(img)} />
     </div>
   );
---
+++ src/components/calendar/CalendarIndex.tsx
@@ -0,0 +1,39 @@
+import { useCalendarApp, ScheduleXCalendar } from "@schedule-x/react";
+import {
+  createViewDay,
+  createViewMonthAgenda,
+  createViewMonthGrid,
+  createViewWeek,
+} from "@schedule-x/calendar";
+import { useState } from "react";
+import { createEventsServicePlugin } from "@schedule-x/events-service";
+
+import "@schedule-x/theme-default/dist/index.css";
+
+export const CalendarIndex = () => {
+  const eventsService = useState(() => createEventsServicePlugin())[0];
+
+  const calendar = useCalendarApp({
+    views: [
+      createViewDay(),
+      createViewWeek(),
+      createViewMonthGrid(),
+      createViewMonthAgenda(),
+    ],
+    events: [
+      {
+        id: "1",
+        title: "Event 1",
+        start: "2023-12-16",
+        end: "2023-12-16",
+      },
+    ],
+    plugins: [eventsService],
+  });
+
+  return (
+    <div>
+      <ScheduleXCalendar calendarApp={calendar} />
+    </div>
+  );
+};
